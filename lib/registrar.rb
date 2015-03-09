af3f9560cc5edd9e10993a8d92ece5c62c2f266b
Jan Owiesniak <jowiesniak@gmail.com>
2015-03-09 17:12:19 +0100

    Get rid of ProfileGatewayStub

--- lib/registrar.rb
+++ lib/registrar.rb
@@ -1,7 +1,6 @@
 require "registrar/version"
 require 'registrar/adapter'
 require 'registrar/profile_builder'
-require 'registrar/profile_gateway_stub'
 require 'registrar/debug'
 
 module Registrar
