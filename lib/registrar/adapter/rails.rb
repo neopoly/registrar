f6b48b2e768c2d4de7285975b3449621b119f63b
Jan Owiesniak <jowiesniak@gmail.com>
2015-03-17 14:18:55 +0100

    Add additional helper methods to Rails Adapter

--- lib/registrar/adapter/rails.rb
+++ lib/registrar/adapter/rails.rb
@@ -7,6 +7,19 @@ module Registrar
       def self.included(klass)
         klass.include InstanceMethods
         klass.before_action :try_to_store_registrar_profile
+
+        klass.class_eval do
+          helper_method :current_profile
+          helper_method :current_profile?
+          helper_method :logged_in?
+
+          helper_method :registrar_profile
+          helper_method :registrar_profile?
+
+          helper_method :authentication_phase?
+
+          helper_method :presentable_authentication
+        end
       end
 
       module InstanceMethods
@@ -27,6 +40,11 @@ module Registrar
           try_to_set_current_profile
         end
 
+        def current_profile?
+          !!current_profile
+        end
+        alias_method :logged_in?, :current_profile?
+
         def try_to_set_current_profile
           if registrar_profile?
             @current_user = build_profile(registrar_profile)
@@ -43,6 +61,20 @@ module Registrar
 
         def build_profile(profile)
           ::Registrar::Profile.new(profile)
+        end
+
+        def authentication_phase?
+          params[:controller] == 'authentication' && params[:action] = 'callback'
+        end
+
+        def presentable_authentication
+          {
+            'env.omniauth.auth' => request.env['omniauth.auth'],
+            'env.registrar.auth' => request.env['registrar.auth'],
+            'env.registrar.profile' => request.env['registrar.profile'],
+            'session.registrar_profile' => registrar_profile,
+            'runtime.current_profile' => current_profile
+          }
         end
       end
     end
