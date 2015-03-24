7feaea1a8bf20eba627ffb46e188a9fe8fb0daba
Jan Owiesniak <jowiesniak@gmail.com>
2015-03-24 13:10:09 +0100

    Params adapter overwrite params as well

--- lib/registrar/adapter/params.rb
+++ lib/registrar/adapter/params.rb
@@ -7,16 +7,27 @@ module Registrar
       end
 
       def call(env)
-        @mapping.each do |tupel, attribute|
-          value = request(env).params[attribute]
-          namespace, attr = tupel.split('#')
-          params(env)[namespace][attr] = value
-        end
+        build_registrar_params(env)
+        overwrite_params(env)
 
         @app.call(env)
       end
 
       private
+
+      def build_registrar_params(env)
+        @mapping.each do |tupel, attribute|
+          value = request(env).params[attribute]
+          namespace, attr = tupel.split('#')
+          params(env)[namespace][attr] = value
+        end
+      end
+
+      def overwrite_params(env)
+        params(env).each do |namespace, values|
+          request(env).update_param(namespace, values)
+        end
+      end
 
       def request(env)
         @request ||= Rack::Request.new(env)
--- spec/params_adapter_spec.rb
+++ spec/params_adapter_spec.rb
@@ -2,32 +2,37 @@ require 'spec_helper'
 require 'omniauth'
 
 class ParamsAdapterSpec < Spec
+  it 'stores normalized params in registrar.params' do
+    get '/'
+    assert_normalizes_params env['registrar.params']
+  end
+
   it 'normalizes params to registrar schema' do
     get '/'
-    assert_normalizes_params env
+    assert_normalizes_params Rack::Request.new(env).params
   end
 
   private
 
-  def assert_normalizes_params(env)
-    assert_normalizes_provider(env)
-    assert_normalizes_profile(env)
+  def assert_normalizes_params(params)
+    assert_normalizes_provider(params)
+    assert_normalizes_profile(params)
   end
 
-  def assert_normalizes_provider(env)
+  def assert_normalizes_provider(params)
     assert_equal(
       {
         'name' => 'facebook',
         'uid' => '100000100277322',
-      }, env['registrar.params']['provider'])
+      }, params['provider'])
   end
 
-  def assert_normalizes_profile(env)
+  def assert_normalizes_profile(params)
     assert_equal(
       {
         "name" => "Jan Ow",
         "email" => "jan@featurefabrik.de",
-      }, env['registrar.params']['profile'])
+      }, params['profile'])
   end
 
   def env
