5acbfac44d3b7754e503703f2f4b5372cfa34199
Jan Owiesniak <jowiesniak@gmail.com>
2015-03-24 16:48:56 +0100

    Remove obsolete Registrar::Gatekeeper

--- lib/registrar.rb
+++ lib/registrar.rb
@@ -2,7 +2,6 @@ require "registrar/version"
 require 'registrar/adapter'
 require 'registrar/mapper'
 require 'registrar/auth_builder'
-require 'registrar/gatekeeper'
 require 'registrar/middleware'
 require 'registrar/profile_builder'
 require 'registrar/debug'
